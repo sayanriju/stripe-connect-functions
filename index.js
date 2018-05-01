eb76e4665c37541d98d1342179bdedc12842a9ba
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2018-05-01 18:15:11 +0530

    Documentation done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Stripe = require("stripe")
-
 module.exports = (stripeSecretKey) => {
   const stripe = Stripe(stripeSecretKey)
   return {
+    /**
+     * Create a Customer on Stripe
+     * @param {string} [email=null]
+     * @returns {Promise}
+     */
     async customerCreate(email = null) {
       try {
         return await stripe.customers.create({ email })
@@ -12,6 +16,11 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Create a Vendor (Custom Account on Stripe)
+     * @param {string} [email=null]
+     * @returns {Promise}
+     */
     async customAccountCreate(email = null) {
       try {
         return await stripe.accounts.create({
@@ -25,6 +34,11 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Fetch all the Cards (or other sources) associated with the Customer with the provided id. The default card is marked as such.
+     * @param {string} stripeCustomerId Customer Id (begins with "cus_")
+     * @returns {Promise}
+     */
     async fetchCustomerCards(stripeCustomerId) {
       try {
         const result = await Promise.all([
@@ -42,6 +56,12 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+   * Add a Card to a Customer
+   * @param {string} stripeCustomerId Id of the Customer to whom the Card is to be added (begins with "cus_")
+   * @param {string} stripeToken The token representing the Card to add, genetrated client-side using Stripe.js, etc.
+   * @returns {Promise}
+   */
     async addCustomerCard(stripeCustomerId, stripeToken) {
       try {
         return await stripe.customers.createSource(stripeCustomerId, {
@@ -53,6 +73,12 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Delete a Customer's Card
+     * @param {string} stripeCustomerId Id of the Customer for whom the Card is to be deleted (begins with "cus_")
+     * @param {string} cardId Id of the Card to be deleted
+     * @returns {Promise}
+     */
     async deleteCustomerCard(stripeCustomerId, cardId) {
       try {
         return await stripe
@@ -64,6 +90,11 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Get a Customer's default Card. Note that, this info is already available implicitly from `fetchCustomerCards()`
+     * @param {string} stripeCustomerId Customer Id (begins with "cus_")
+     * @returns {Promise}
+     */
     async getDefaultCustomerCard(stripeCustomerId) {
       try {
         const {
@@ -76,6 +107,12 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Set an existing Customer Card as the default payment source
+     * @param {string} stripeCustomerId Customer Id (begins with "cus_")
+     * @param {string} stripeCardId The id of the Card to be set as default. Must be one of the Customer's existing Cards.
+     * @returns {Promise}
+     */
     async setDefaultCustomerCard(stripeCustomerId, stripeCardId) {
       try {
         const {
@@ -90,6 +127,15 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Set the default Bank A/c for a Vendor (Custom Account on Stripe)
+     * @param {string} stripeAccountId Vendor Id (begins with "acct_")
+     * @param {object} _ The Bank A/c details
+     * @param {string}  _.routingNo
+     * @param {string} _.accountNo
+     * @param {string} [_.accountHolderName]
+     * @returns {Promise}
+     */
     async setVendorBankAccount(stripeAccountId, {
       routingNo,
       accountNo,
@@ -115,6 +161,28 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Update KYC details for a Vendor (Stripe Custom Account). These are mandatory to be able to receive payments and payouts.
+     * @param {string} stripeAccountId Vendor Id (begins with "acct_")
+     * @param {object} _ The KYC details
+     * @param {object} _.address Address details
+     * @param {string} _.address.city City
+     * @param {string} _.address.line1 Address Line 1
+     * @param {string} _.address.line2 Address Line 2
+     * @param {string} _.address.postal_code ZIP/Postal Code
+     * @param {string} _.address.state State/County/Province/Region
+     * @param {string} _.address.country 2-letter Country Code
+     * @param {object} _.dob Date of Birth
+     * @param {string} _.dob.day Day of Birth
+     * @param {string} _.dob.month Month of Birth
+     * @param {string} _.dob.year Year of Birth
+     * @param {object} _.name Name
+     * @param {string} _.name.first First Name
+     * @param {string} _.name.last Last Name
+     * @param {string} _.ssnLastFour Last 4 digits of the SSN
+     * @param {string} _.fullSsn The full SSN
+     * @returns {Promise}
+     */
     async vendorKyc(stripeAccountId, {
       address = {},
       dob = {},
@@ -141,6 +209,15 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * ToS acceptance of a Vendor (Stripe Custom Account)
+     * @param {string} stripeAccountId Vendor Id (begins with "acct_")
+     * @param {object} _ ToS Acceptance details
+     * @param {string} _.tosAcceptanceDate Date (in UNIX timestamp format) of ToS acceptance
+     * @param {string} _.tosAcceptanceIp IP address from where the ToS was accepted
+     * @param {string} [_.tosUserAgent=null] User Agent string of the browser using which the ToS was accepted
+     * @returns {Promise}
+     */
     async vendorAcceptTos(stripeAccountId, {
       tosAcceptanceDate,
       tosAcceptanceIp,
@@ -161,6 +238,20 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Initiate a Payment (Stripe Charge creation)
+     * @param {object} _ Payment (Stripe Charge) parameters
+     * @param {boolean} [_.capture=false] Whether to capture this charge later or immediately. By default, it is set to false, signifying later capture. Note that such charges need to be captured manually within 7 days, beyond which it is automatically reveresed/refunded by Stripe.
+     * @param {string} _.customer Id of the Customer (Stripe Customer) making the payment (begins with "cus_")
+     * @param {string} _.vendor Id of the Vendor (Stripe Custom Account) to receive the payment (begins with "acct_")
+     * @param {number} _.amount Amount to deduct from Customer
+     * @param {number} _.vendorAmount Amount payable to the Vendor. Must be less than or equal to `amount`. Ideally, the difference between `amount` & `vendorAmount`, minus the Stripe fees, is what the Marketplace retains as profit.
+     * @param {string} _.currency Currency code
+     * @param {string} [_.receiptEmail=null] Email to mail the receipt from Stripe
+     * @param {string} [_.description] Optional description
+     * @param {string} [_.statementDescriptor] Text appearing on Bank/Card statements
+     * @returns {Promise}
+     */
     async initiatePayment({
       customer,
       vendor,
@@ -192,6 +283,13 @@ module.exports = (stripeSecretKey) => {
       }
     },
 
+    /**
+     * Capture an existing Payment (Stripe Charge)
+     * @param {string} transactionId Id of the Stripe Charge to capture (begins with "ch_")
+     * @param {number} [vendorAmount=null] Optionally update the amount payable to Vendor. If not mentioned, the amount in original charge is used.
+     * @param {string} [statementDescriptor] Text appearing on Bank/Card statements (overrides the one mentioned in original charge)
+     * @returns {Promise}
+     */
     async capturePayment(
       transactionId,
       vendorAmount = null,
