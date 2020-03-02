68eba81268605d12d73d501c85beaecef0c90e7b
Indrajit Roy <developerindrajit@gmail.com>
2020-03-02 11:39:24 +0530

    Updated test cases according to changes

--- test/_stripe.stub.js
+++ test/_stripe.stub.js
@@ -16,56 +16,163 @@ module.exports = () => ({
       })
     },
 
-    update(acctId, { external_account = {}, legal_entity = {}, tos_acceptance = {} }) {
+    update(acctId, {
+      external_account = {},
+      business_profile = {},
+      tos_acceptance = {},
+      individual = {}
+    }) {
       return Promise.resolve({
         id: acctId,
         object: "account",
-        type: "custom",
+        business_profile: {
+          mcc: business_profile.mcc || null,
+          name: null,
+          product_description: null,
+          support_address: null,
+          support_email: null,
+          support_phone: null,
+          support_url: null,
+          url: business_profile.url || null
+        },
+        business_type: "individual",
+        capabilities: {
+          transfers: "active"
+        },
+        charges_enabled: true,
         country: "US",
+        created: 1582800920,
         default_currency: "usd",
-        email: "sayan@logic-square.com",
+        details_submitted: true,
+        email: "indrajit@logic-square.com",
         external_accounts: {
           object: "list",
           data: [
             {
+              id: "ba_1GGk1VL1F8SCaTrVwklLM6H5",
               object: "bank_account",
-              country: "US",
-              currency: "usd",
-              account_number: external_account.account_number || null,
+              account: acctId,
+              account_holder_name: external_account.account_holder_name || null,
+              account_holder_type: null,
+              bank_name: "STRIPE TEST BANK",
+              country: external_account.country || null,
+              currency: external_account.currency || null,
+              default_for_currency: true,
+              fingerprint: "0cXClcKtgVTOE0iW",
+              last4: external_account.account_number ? external_account.account_number.substr(-4) : null,
+              metadata: {},
               routing_number: external_account.routing_number || null,
+              status: "new"
             }
           ],
           has_more: false,
-          total_count: 0,
-          url: "/v1/accounts/acct_1CK10nIacGIwwFOI/external_accounts"
+          total_count: 1,
+          url: "/v1/accounts/acct_1GGk1UL1F8SCaTrV/external_accounts"
         },
-        legal_entity: {
+        individual: {
+          id: "person_GoMlL8zwfPV0UZ",
+          object: "person",
+          account: "acct_1GGk1UL1F8SCaTrV",
           address: {
-            city: "New York",
-            country: "US",
-            line1: "Add Line 1",
-            line2: "Add Line 2",
-            postal_code: "700001",
-            state: "NY"
+            city: individual.address && individual.address.city ? individual.address.city : null,
+            country: individual.address && individual.address.country ? individual.address.country : null,
+            line1: individual.address && individual.address.line1 ? individual.address.line1 : null,
+            line2: individual.address && individual.address.line2 ? individual.address.line2 : null,
+            postal_code: individual.address && individual.address.postal_code ? individual.address.postal_code : null,
+            state: individual.address && individual.address.state ? individual.address.state : null
           },
+          created: 1582800923,
           dob: {
-            day: "13",
-            month: "05",
-            year: "1990"
+            day: individual.dob && individual.dob.day ? individual.dob.day : null,
+            month: individual.dob && individual.dob.month ? individual.dob.month : null,
+            year: individual.dob && individual.dob.year ? individual.dob.year : null
           },
-          first_name: "Foo",
-          last_name: "Bar",
-          ssn_last_4_provided: !!legal_entity.ssn_last_4,
-          ssn_last_4: legal_entity.ssn_last_4 || null,
-          personal_id_number_provided: !!legal_entity.personal_id_number,
-          personal_id_number: legal_entity.personal_id_number || null,
-          type: null,
+          email: individual.email || null,
+          first_name: individual.first_name || null,
+          id_number_provided: !!individual.id_number,
+          last_name: individual.last_name || null,
+          metadata: {},
+          phone: individual.phone || null,
+          relationship: {
+            director: false,
+            executive: false,
+            owner: false,
+            percent_ownership: null,
+            representative: true,
+            title: null
+          },
+          requirements: {
+            currently_due: [],
+            eventually_due: [],
+            past_due: [],
+            pending_verification: []
+          },
+          ssn_last_4_provided: !!individual.ssn_last_4,
+          verification: {
+            additional_document: {
+              back: null,
+              details: null,
+              details_code: null,
+              front: null
+            },
+            details: null,
+            details_code: null,
+            document: {
+              back: null,
+              details: null,
+              details_code: null,
+              front: null
+            },
+            status: "pending"
+          }
+        },
+        metadata: {},
+        payouts_enabled: true,
+        requirements: {
+          current_deadline: null,
+          currently_due: [],
+          disabled_reason: null,
+          eventually_due: [],
+          past_due: [],
+          pending_verification: []
+        },
+        settings: {
+          branding: {
+            icon: null,
+            logo: null,
+            primary_color: null
+          },
+          card_payments: {
+            decline_on: {
+              avs_failure: false,
+              cvc_failure: false
+            },
+            statement_descriptor_prefix: null
+          },
+          dashboard: {
+            display_name: "Example",
+            timezone: "Etc/UTC"
+          },
+          payments: {
+            statement_descriptor: "WWW.EXAMPLE.COM",
+            statement_descriptor_kana: null,
+            statement_descriptor_kanji: null
+          },
+          payouts: {
+            debit_negative_balances: false,
+            schedule: {
+              delay_days: 2,
+              interval: "daily"
+            },
+            statement_descriptor: null
+          }
         },
         tos_acceptance: {
           date: tos_acceptance.date || null,
           ip: tos_acceptance.ip || null,
           user_agent: tos_acceptance.user_agent || null
-        }
+        },
+        type: "custom"
       })
     }
   },
